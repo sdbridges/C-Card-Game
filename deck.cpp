ea375991f6daed2b3aa8cdac5a839e794e78a2a1
Stuart Bridges <46537662+sdbridges@users.noreply.github.com>
2022-04-13 15:36:01 -0400

    Update deck.cpp

--- deck.cpp
+++ deck.cpp
@@ -1,71 +1,51 @@
 #include <iostream>
-#include <algorithm>
 #include <random>
 
 #include "deck.h"
 
-Deck::Deck():deck() {
-    deck.reserve(noOfCards);  //buffer 52 bytes
+static std::random_device rd;
+static std::mt19937 rng(rd());
 
+Deck::Deck()
+{
     for (int suit = 0; suit < 4; suit++) {
         for (int value = 0; value < 13; value++) {
             Card* card = new Card(static_cast<Card::Suit>(suit), static_cast<Card::Value>(value));
-            this -> deck.push_back(card);
-//            std::cout << card->getValue() << "  " <<  &card[value]  << std::endl;
-//            deck.push_back(new Card(static_cast<Card::Suit>(suit), static_cast<Card::Value>(value)));
+            this-> full_deck.push_back(card);
         }
     }
 }
 
-void Deck::shuffle() {
-
-    //clear the stack
-    while (!stack.empty()) {
-        stack.top() = NULL; //clear data
-        stack.pop();  //pop node from stack
-    }
-
-    //shuffle the deck using algorithm
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::shuffle(deck.begin(), deck.end(), std::default_random_engine(seed));
-
-    //repopulate stack with full deck (pointers)
-    for (int i = 0; i < noOfCards; i++) {
-        stack.push(deck[i]);
+Deck::~Deck()
+{
+    for (int x = 0; x < this->full_deck.size(); x++) {
+        delete this->full_deck[x];
     }
 }
 
-Card* Deck::draw() {
+void Deck::shuffle()
+{
+    while (this->draw_deck.size() > 0) {
+        this->draw_deck.pop();
+    }
 
-    //receive a pointer to the Card on top
-    int cardSuit = stack.top()->getSuit();
-    int cardValue = stack.top()->getValue();
-    Card* card = new Card(static_cast<Card::Suit>(cardSuit), static_cast<Card::Value>(cardValue));
+    std::shuffle(this->full_deck.begin(), this->full_deck.end(), rng);
 
-    stack.top() = NULL;  //clears data on top card
-    stack.pop();  //remove node
-    return card;
-
-}
-
-void Deck::printDeck() {
-    for (Card* card : deck) {
-        card->printCard();
+    for (int x = 0; x < this->full_deck.size(); x++) {
+        this->draw_deck.push(this->full_deck[x]);
     }
 }
 
-Deck::~Deck() {
-    // free the memory associated with each Card instance
-    for (Card* card : deck) {
-        delete card;
+Card* Deck::draw()
+{
+    Card *draw_card = NULL;
+
+    if (this->draw_deck.empty()) {
+        return draw_card;
     }
 
-    while (!stack.empty()) {
-        stack.top() = NULL;
-        stack.pop();
-    }
+    draw_card = this->draw_deck.top();
+    this->draw_deck.pop();
 
-    deck.clear();
+    return draw_card;
 }
-
-
